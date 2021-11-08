63265f492edfa110ef616f715997d2c5e28293cb
Vanya Startsev <vanya.startsev.60@mail.ru>
2021-11-08 18:34:13 +0500

    Second commit

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import sys
-from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from random import randint
+from Ui import Ui_MainWindow
 
 
-class DrawStar(QMainWindow):
+class DrawStar(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.draww)
         self.draw = False
 
@@ -17,8 +16,8 @@ class DrawStar(QMainWindow):
         if self.draw:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(Qt.yellow))
             for i in range(randint(1, 100)):
+                qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
                 r = randint(1, 100)
                 qp.drawEllipse(randint(1, 1000), randint(1, 1000), r, r)
             self.draw = False
